9af77f2ebd24778609df52561ef327f29bf636a7
Ayush Arora <ayusharora009@gmail.com>
2019-03-08 00:05:53 +0530

    adding array like numbers

--- test.js
+++ test.js
@@ -1,26 +1,33 @@
 // https://leetcode.com/problems/add-to-array-form-of-integer/
 // OneNote notebook : https://1drv.ms/u/s!Al8jX5-HB-eXgYcEZHkf-_cZZeb8Cg
-(()=>{
-	let a = [1,2,0,0]
-	global.b = [3,4]
-	// let len = Math.max(a.length,b.length)
-	let c = new Array();
-	let digit_sum = undefined;
-	let carry = 0;
-	console.log("b", b);
-	for (let i=a.length;i>0;i--){
-		console.log("i",i);
-		console.log("global.b", global.b);
-		let b = global.b[i] ? global.b[i] : 0;
-		digit_sum = a[i]+b;
-		// if(digit_sum>10){
-		// 	let mod = ; digit_sum % 10;
-		// 	carry =  parseInt(digit_sum / 10);
-		// 	c.push(mod);
-		// }
-		// else
+
+var a = [2,1,5]
+var b = 806;
+let c = new Array();
+let digit_sum = undefined;
+let carry = 0;
+
+a = a.reverse();
+b = b.toString().split("").reverse();
+var len = Math.max(a.length,b.length)
+
+for (let i=0;i<len;i++){
+	if(!a[i])
+		a.push(0)
+	if(!b[i])
+		b.push(0)
+	digit_sum = a[i]+ parseInt(b[i]) + carry;
+	if(digit_sum>=10){
+		let mod = digit_sum % 10;
+		carry =  parseInt(digit_sum / 10);
+		c.push(mod);
+	}
+	else{
+		carry = 0;
 		c.push(digit_sum)
 	}
-	console.log("c", c);
-})();
+}
+if(carry)
+	c.push(carry)
+console.log("c", c.reverse());
 
